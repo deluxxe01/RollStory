ac89d439912746af4bc9e1d0ccb3d240f00bb157
deluxxe01 <dudaklincarvalho@gmail.com>
2025-02-17 11:29:20 -0300

    atualizado segunda

--- src/components/Roll.jsx
+++ src/components/Roll.jsx
@@ -1,25 +1,26 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react"; //usar a sintaxe JSX 
 import './Roll.css'
 
 function Roll() {
-    const [emoji, setEmoji] = useState('Clique aqui')
-    const emojis = ['🤣', '❤', '😍', '😜'];
+    //CRIAÇÃO DE ESTADOS
+    const [emoji, setEmoji] = useState('Clique aqui'); //função setEmoji para atualizar o valor do emoji
+    const [usados, setUsados] = useState([]);  // Adiciona o estado para os usados
+    const emojis = ['🤣', '❤', '😍', '😜']; // arrey para sortear emoji
 
     function sortear() {
-        let n = Math.floor(Math.random()* emojis.length )
-        setEmoji(emojis[n])
+        //FUNÇÃO SORTEAR
+        let n = Math.floor(Math.random() * emojis.length); //Gerar um numero aleatorio no comprimento do arrey 
+        const emojiSorteado = emojis[n];// valor do emoji é atualizado com um valor do arrey emojis 
+        setEmoji(emojiSorteado); // atualizar estado emoji com o novo valor do emojiSorteado fazendo com que mude a interface dobotão
 
-        setUsados([...usados, atual])
-
+        setUsados([...usados, emojiSorteado]); //adiciona o emoji sorteado à lista de usados
     }
 
     return (
         <div className="container-Roll">
-            <button onClick={sortear}> {emoji}</button>
-            
+            <button onClick={sortear}>{emoji}</button>
         </div>
-    )
+    );
 }
 
-export default Roll
+export default Roll;
